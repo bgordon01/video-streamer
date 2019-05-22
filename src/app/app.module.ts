a1a34fe798afac8ceec97feb573c42e93fa606d0
Brent Gordon <brentgordon01@gmail.com>
2019-05-22 13:36:58 +0200

    Modified the main app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { SeriesComponent } from './series/series.component';
+import { LoadingDirective } from './loading.directive';
+import { ErrorDirective } from './error.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MoviesComponent,
+    SeriesComponent,
+    LoadingDirective,
+    ErrorDirective
   ],
   imports: [
     BrowserModule,
