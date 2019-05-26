68ddfc68259ffb5219552f129352e5b800324387
Brent Gordon <brentgordon01@gmail.com>
2019-05-26 17:58:56 +0200

    Added the series listing functionality

--- src/app/components/series/series.component.ts
+++ src/app/components/series/series.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { VideoService } from '../../services/video.service';
+import { SearchOptions, Video } from '../../models/video';
 
 @Component({
   selector: 'app-series',
@@ -9,13 +11,41 @@ import { ActivatedRoute } from '@angular/router';
 export class SeriesComponent implements OnInit {
 
 	pageTitle: string;
+	series: Video[];
 	
-	constructor(private activatedRoute: ActivatedRoute) {}
+	constructor(
+		private activatedRoute: ActivatedRoute,
+		private videosService: VideoService
+	) {}
 
 	ngOnInit() {
 		this.activatedRoute.data.subscribe(values => {
 			this.pageTitle = values['title'];
 		});
+		this.getSeries();
+	}
+
+	getSeries(): void {
+		this.series = this.videosService.search({
+			filters: [{
+				field: 'programType',
+				operator: 'equalTo',
+				value: 'series'
+			},{
+				field: 'releaseYear',
+				operator: 'greaterEqualTo',
+				value: '2010'
+			}],
+			sort: [{
+				field: 'title',
+				order: 'asc'
+			}],
+			limitTo: 21
+		} as SearchOptions);
+	}
+
+	getThumbnail(movie: Video) {
+		return movie.images['Poster Art'].url;
 	}
 
 }
