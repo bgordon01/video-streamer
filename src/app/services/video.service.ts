e99d591d2098c85f154ef424040d0010dd8d0c1f
Brent Gordon <brentgordon01@gmail.com>
2019-05-27 17:08:59 +0200

    Some clean up

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -193,18 +193,21 @@ export class VideoService {
 	private handleError<T>(operation = 'operation', result?: T) {
 		return (error: any): Observable<T> => {
 
-			// TODO: send the error to remote logging infrastructure
-			console.error(error); // log to console instead
+			console.error(error); 
 
-			// TODO: better job of transforming error for user consumption
 			this.log(`${operation} failed: ${error.message}`);
 
-			// Let the app keep running by returning an empty result.
 			return of(result as T);
 		};
 	}
-
-	/** Log a HeroService message with the MessageService */
+	
+	/**
+	 * Log a VideoService message 
+	 *
+	 * @private
+	 * @param {string} message
+	 * @memberof VideoService
+	 */
 	private log(message: string) {
 		console.log(`VideoService: ${message}`);
 	}
